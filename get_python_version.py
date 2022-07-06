e383633d185c5c712386be6035e0361462cbdeb6
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-06 09:12:23 +0200

    Create get_python_version.py
    Get the version of Python installed on the executing machine.

---
+++ get_python_version.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# get_python_version.py
+
+# Dependency
+import sys
+
+# Get the version of Python installed on the executing machine.
+def get_version():
+    return sys.version
+
+if __name__ == '__main__':
+    get_version()
